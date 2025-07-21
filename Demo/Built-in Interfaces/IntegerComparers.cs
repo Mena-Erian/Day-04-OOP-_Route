a85bc503e4aa841a3e5afbc9d00a9771a34df66a
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-21 17:06:15 +0300

    Part 09 Built-In Interface IComparer

---
+++ Demo/Built-in Interfaces/IntegerComparers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Built_in_Interfaces
+{
+    internal class IntegerComparers : IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            int? NumberX = (int?)x;
+            int? NumberY = (int?)y;
+
+            //return NumberX is null ? 0 : -1
+            return -NumberX.Value.CompareTo(NumberY.Value) /*?? (NumberY is null? 0: -1)*/;
+            //return -NumberX?.Value.CompareTo(NumberY?.Value) ?? (NumberY is null? 0: -1);
+
+        }
+    }
+}
