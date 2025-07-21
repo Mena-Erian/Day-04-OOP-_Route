646a79ad27478f3350258c057c003346c58338a0
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-21 14:53:39 +0300

    Part 05 Deep Copy Vs Shallow Copy

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -18,7 +18,7 @@ namespace Demo
             }
             series.Reset();
             Console.WriteLine();
-        } 
+        }
         #endregion
         static void Main()
         {
@@ -58,23 +58,63 @@ namespace Demo
             #endregion
 
             #region Interface Example 03
-
-            Airplane airplane = new Airplane();
-            airplane.Speed = 480;
-            //airplane.Frowerd();
-
-            IMoveable moveable = airplane;
-            moveable.Forward();
-
-            IFlyable flyable = airplane;
-            flyable.Forward();
-
-
-
-
-
-
+            /// Airplane airplane = new Airplane();
+            /// airplane.Speed = 480;
+            /// //airplane.Frowerd();
+            /// 
+            /// IMoveable moveable = airplane;
+            /// moveable.Forward();
+            /// 
+            /// IFlyable flyable = airplane;
+            /// flyable.Forward();
             #endregion
+
+            #region Shallow Copy VS Deep Copy
+            /// int[] Arr01 = [1, 2, 3];
+            /// int[] Arr02 = [4, 5, 6];
+            /// 
+            /// Console.WriteLine($"Arr01.GetHashCode() => {Arr01.GetHashCode()}"); // Based in his place in heap
+            /// Console.WriteLine($"Arr02.GetHashCode() => {Arr02.GetHashCode()}");
+
+            #region Shallow Copy
+            /// Arr02 = Arr01;
+            /// /// Shallow Copy [سطحى]
+            /// /// This Object [1, 2, 3] has 2 Reference [Arr01 , Arr02]
+            /// /// This Object [4, 5, 6] Became UnReachable Object
+            /// Console.WriteLine($"Arr01.GetHashCode() => {Arr01.GetHashCode()}"); // Based in his place in heap
+            /// Console.WriteLine($"Arr02.GetHashCode() => {Arr02.GetHashCode()}");
+            /// 
+            /// Arr02[0] = 100;// after shallow copy change in arr02 it will be changed in original also (arr01)
+            /// Console.WriteLine(Arr01[0]); // 100
+            #endregion
+
+            #region Deep Copy
+            /// Arr02 = (int[])Arr01.Clone();
+            /// Console.WriteLine("Deep Copy --> Arr02 = (int[])Arr01.Clone();");
+            /// // Deep Copy
+            /// // Clone Method: Will Generate NEW Object with NEW and DIFFERENT Identity
+            /// //             : This Object Will Have The Same State [Data] of the Caller Object
+            /// 
+            /// Console.WriteLine($"Arr01.GetHashCode() => {Arr01.GetHashCode()}"); // Based in his place in heap
+            /// Console.WriteLine($"Arr02.GetHashCode() => {Arr02.GetHashCode()}");
+            /// 
+            /// Arr02[0] = 100;// after Deep copy change in arr02 it will be not changed in original (arr01)
+            /// Console.WriteLine(Arr01[0]); // 1
+            /// Console.WriteLine("-------------------");
+            /// for (int i = 0; i < Arr01.Length; i++)
+            /// {
+            ///     Console.WriteLine(Arr01[i]); // 1
+            /// }
+            /// Console.WriteLine("******************************");
+            /// for (int i = 0; i < Arr02.Length; i++)
+            /// {
+            ///     Console.WriteLine(Arr02[i]); // 1
+            /// }
+            #endregion
+            #endregion
+
+
+
         }
     }
 }
