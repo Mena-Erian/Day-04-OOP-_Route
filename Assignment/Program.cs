b4f0a168de81546faef2a25e5a78ecc931b7613c
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-22 10:35:11 +0300

    Done Part 01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,23 +4,52 @@
     {
         static void Main()
         {
+            #region Part 01
+
             #region Q1
-            //b) To define  a blueprint for a class
+            // b) To define  a blueprint for a class
             #endregion
 
             #region Q2
-            //b) protected
+            // b) protected
             #endregion
 
             #region Q3
-            //b) No
+            // b) No
             #endregion
 
             #region Q4
-            //b) Yes, interfaces can inherit from multiple interfaces
+            // b) Yes, interfaces can inherit from multiple interfaces
             #endregion
-           
 
+            #region Q5
+            // d) implements
+            #endregion
+
+            #region Q6
+            // a) Yes        (the Default Methods)
+            #endregion
+
+            #region Q7
+            // a) Yes, for all members
+            #endregion
+
+            ///Recap Here---8
+            #region Q8
+            // b) To provide a clear separation between interface and class members
+            #endregion
+
+            #region Q9
+            // d) Only if the constructor is static
+            #endregion
+
+            #region Q10
+            // c) By separating interface names with commas
+            #endregion
+
+            #endregion
+        
+            
         }
     }
 }
